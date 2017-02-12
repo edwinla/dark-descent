051c0f5f04a5241ec515466c78bb91168ef75733
laed37 <La.Edwin37@gmail.com>
2017-02-12 00:22:03 -0800

    Add acquire neighbors fn to node class

--- map_node.js
+++ map_node.js
@@ -1,8 +1,26 @@
 export default class MapNode {
-  constructor(yCoord, xCoord) {
+  constructor(yCoord, xCoord, tile) {
     this.yCoord = yCoord;
     this.xCoord = xCoord;
+    this.tile = tile;
+    this.parent = null;
   }
 
-  
+  getNeighbors(nodes) {
+    const neighbors = [];
+    if (this.yCoord > 0) {
+      neighbors.push(nodes[this.yCoord - 1][this.xCoord]);
+    }
+    if (this.yCoord < nodes.length) {
+      neighbors.push(nodes[this.yCoord + 1][this.xCoord]);
+    }
+    if (this.xCoord > 0) {
+      neighbors.push(nodes[this.yCoord][this.xCoord - 1]);
+    }
+    if (this.xCoord < nodes[0].length) {
+      neighbors.push(nodes[this.yCoord][this.xCoord + 1]);
+    }
+
+    return neighbors;
+  }
 }
