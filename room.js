6d46b02004148f54e507f6ffe94892b615675e70
laed37 <La.Edwin37@gmail.com>
2017-02-12 17:16:17 -0800

    Remove edges on room generation

--- room.js
+++ room.js
@@ -5,15 +5,46 @@ export default class Room {
     this.map = map;
     this.absolutePosition = this.getRandomPosition(map[0].length, map.length);
     this.corners = this.getCorners();
+    this.edges = this.linearizeEdges();
+    this.door = null;
   }
 
   getRandomPosition(mapWidth, mapHeight) {
-    const xUpperLimit = mapWidth - this.width - 1;
-    const yUpperLimit = mapHeight - this.height - 1;
+    const xUpperLimit = mapWidth - this.width - 3;
+    const yUpperLimit = mapHeight - this.height - 3;
     return {
-      x: this.getRandomNumber(1, xUpperLimit),
-      y: this.getRandomNumber(1, yUpperLimit)
+      x: this.getRandomNumber(3, xUpperLimit),
+      y: this.getRandomNumber(3, yUpperLimit)
     };
+  }
+
+  generateDoorLocation() {
+    this.door = this.edges[Math.floor(Math.random() * (this.edges.length + 1))];
+    return this.door;
+  }
+
+  linearizeEdges() {
+    const absX = this.absolutePosition.x, absY = this.absolutePosition.y;
+    const absXLimit = absX + this.width - 1;
+    const absYLimit = absY + this.height - 1;
+
+    const edges = [];
+
+    for (let y = absY; y <= absYLimit; y += (this.height - 1)) {
+      for (let x = absX; x <= absXLimit; x++) {
+        const node = this.map[y][x];
+        edges.push(node);
+      }
+    }
+
+    for (let y = absY + 1; y < absYLimit; y++) {
+      for (let x = absX; x <= absXLimit; x += (this.width - 1)) {
+        const node = this.map[y][x];
+        edges.push(node);
+      }
+    }
+
+    return edges;
   }
 
   updateNodes() {
@@ -23,40 +54,44 @@ export default class Room {
     for (let y = absY ; y <=  absYLimit; y++) {
       for (let x = absX ; x <= absXLimit; x++) {
         const node = this.map[y][x];
-        if (y === absY) {
-          switch (x) {
-            case (absX):
-              node.type = 'd1';
-              break;
-            case (absXLimit):
-              node.type = 'd3';
-              break;
-            default:
-              node.type = 'd2';
-          }
-        } else if (y === absYLimit) {
-          switch (x) {
-            case (absX):
-              node.type = 'd7';
-              break;
-            case (absXLimit):
-              node.type = 'd9';
-              break;
-            default:
-              node.type = 'd8';
-          }
-        } else {
-          switch (x) {
-            case (absX):
-              node.type = 'd4';
-              break;
-            case (absXLimit):
-              node.type = 'd6';
-              break;
-            default:
-              node.type = 'd5';
-          }
-        }
+        node.type = 'd5';
+
+
+
+        // if (y === absY) {
+        //   switch (x) {
+        //     case (absX):
+        //       node.type = 'd1';
+        //       break;
+        //     case (absXLimit):
+        //       node.type = 'd3';
+        //       break;
+        //     default:
+        //       node.type = 'd2';
+        //   }
+        // } else if (y === absYLimit) {
+        //   switch (x) {
+        //     case (absX):
+        //       node.type = 'd7';
+        //       break;
+        //     case (absXLimit):
+        //       node.type = 'd9';
+        //       break;
+        //     default:
+        //       node.type = 'd8';
+        //   }
+        // } else {
+        //   switch (x) {
+        //     case (absX):
+        //       node.type = 'd4';
+        //       break;
+        //     case (absXLimit):
+        //       node.type = 'd6';
+        //       break;
+        //     default:
+        //       node.type = 'd5';
+        //   }
+        // }
       }
     }
   }
@@ -72,20 +107,20 @@ export default class Room {
     // Note: Adding the height/width accounts for the gap size already
     return {
       nw: {
-        x: this.absolutePosition.x - 1,
-        y: this.absolutePosition.y - 1
+        x: this.absolutePosition.x - 2,
+        y: this.absolutePosition.y - 2
       },
       ne: {
-        x: this.absolutePosition.x + this.width,
-        y: this.absolutePosition.y - 1
+        x: this.absolutePosition.x + this.width + 1,
+        y: this.absolutePosition.y - 2
       },
       sw: {
-        x: this.absolutePosition.x - 1,
-        y: this.absolutePosition.y + this.height,
+        x: this.absolutePosition.x - 2,
+        y: this.absolutePosition.y + this.height + 1,
       },
       se: {
-        x: this.absolutePosition.x + this.width,
-        y: this.absolutePosition.y + this.height
+        x: this.absolutePosition.x + this.width + 1,
+        y: this.absolutePosition.y + this.height + 1
       }
     };
   }
