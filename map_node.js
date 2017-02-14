77c92e931278280299f188fadf523fc0c5d77805
laed37 <La.Edwin37@gmail.com>
2017-02-13 23:33:04 -0800

    fix getNeighbors function indexing into null nodes

--- map_node.js
+++ map_node.js
@@ -5,6 +5,7 @@ export default class MapNode {
     this.type = type;
     this.isDoor = false;
     this.parent = null;
+    this.fCost = null;
   }
 
   traversable(map) {
@@ -20,30 +21,46 @@ export default class MapNode {
       [1, 1]
     ];
 
+    // A traversable tile must be water/solid rock, the only exception is if
+    // the tile is a door.
+
+    if (this.isDoor) return true;
+    if (this.type !== 'w1') return false;
+
+    // To prevent the door from moving too close to other rooms we want to have
+    // a sufficient boundary in between.
+
     let doors = 0, boundaries = 0;
 
     for (let i = 0; i < indices.length; i++) {
       const neighbor = map[indices[i][0]][indices[i][1]];
-      if (neighbor.door) doors += 1;
+      if (neighbor.isDoor) doors += 1;
       if (neighbor.type !== 'w1') boundaries += 1;
     }
 
     return (boundaries === 0) || (boundaries < 4 && doors === 1);
   }
 
-  getNeighbors(nodes) {
+  isValidIndex(map, indices) {
+    const yCoord = this.y + indices[0];
+    const xCoord = this.x + indices[1];
+    return (yCoord > 0 && yCoord < map.length) &&
+            (xCoord > 0 && xCoord < map[0].length);
+  }
+
+  getNeighbors(map) {
     const neighbors = [];
     if (this.y > 0) {
-      neighbors.push(nodes[this.y - 1][this.x]);
+      neighbors.push(map[this.y - 1][this.x]);
     }
-    if (this.y < nodes.length) {
-      neighbors.push(nodes[this.y + 1][this.x]);
+    if (this.y < map.length) {
+      neighbors.push(map[this.y + 1][this.x]);
     }
     if (this.x > 0) {
-      neighbors.push(nodes[this.y][this.x - 1]);
+      neighbors.push(map[this.y][this.x - 1]);
     }
-    if (this.x < nodes[0].length) {
-      neighbors.push(nodes[this.y][this.x + 1]);
+    if (this.x < map[0].length) {
+      neighbors.push(map[this.y][this.x + 1]);
     }
 
     return neighbors;
